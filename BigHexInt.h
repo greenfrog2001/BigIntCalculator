8ad9e318e043625bbb91ac9ee5a69ee89fa8150c
greenfrog2001 <53978309+greenfrog2001@users.noreply.github.com>
2021-01-03 10:25:58 +0700

    Final commit
    Adjust subtractions and comments

--- BigHexInt.h
+++ BigHexInt.h
@@ -8,18 +8,21 @@ class BigHexInt {
 		int digits[SIZE] = {}; // Array of max size and full of zeros that will contain the digits
 		int numSigDigits; // Number of significant digits
 		int converted_digits[SIZE] = {}; // int-converted digits array for calculating
+		bool isNegative = false;
 		
 		// Constructor with empty, represents 0
 		BigHexInt () {
 			numSigDigits = 1;
 		}
 		
-		// Constructor with para as an array and its length
-		BigHexInt (int arr[], int arr_length) {
+		// Constructor with 2 parameters: an array, its length and an optional 
+		// parameter is_negative being set default to false
+		BigHexInt (int arr[], int arr_length, bool is_negative = false) {
 			if(arr_length > SIZE) {
 				printf("Wrong input!");
 			}
 			else {
+				isNegative = is_negative;
 				int count = 0;
 				for (int i = 0; i < arr_length; i++) {
 					if (arr[i] != 0) {
@@ -59,6 +62,10 @@ class BigHexInt {
 				}
 				count ++;
 			}
+			if (str[count] == '-') {
+				isNegative = true;
+				count++;
+			}
 			numSigDigits = strlen(str) - count;
 			if (numSigDigits == 0) {
 				numSigDigits = 1;
@@ -91,6 +98,9 @@ class BigHexInt {
 			if (numSigDigits == 1 && digits[SIZE-1] == 0) {
 				printf("0");
 			} else {
+				if (isNegative) {
+					printf("-");
+				}
 				for (int i = SIZE - numSigDigits; i < SIZE; i++) {
 					if (digits[i] >= 0 && digits[i] < 10) {
 						printf("%d", digits[i]);
@@ -107,14 +117,17 @@ class BigHexInt {
 			if (numSigDigits == 1 && digits[SIZE-1] == 0) {
 				printf("0\n");
 			} else {
+				if (isNegative) {
+					printf("-");
+				}
 				for (int i = SIZE - numSigDigits; i < SIZE; i++) {
 						printf("%d\t", converted_digits[i]);
 					}
 				printf("\n");
 			}
 		}
-//		
-		// returns 1 if bigger, -1 if smaller, 0 if equal
+	
+		// Returns 1 if absolute value is bigger, -1 if smaller, 0 if equal
 		int compareTo (BigHexInt other) {
 			if (numSigDigits > other.numSigDigits) {
 				return 1;
@@ -134,39 +147,79 @@ class BigHexInt {
 				return 0;
 			}
 		}
-//		
+		
 		// returns sum of two 2 BigHexInts as a BigHexInt
 		BigHexInt add(BigHexInt other) {
-			int result[SIZE] = {};
-	        int index = SIZE - 1;
-	        int carry = 0;
-	        while (index > 0) {
-	        	int sum = converted_digits[index] + other.converted_digits[index] + carry;
-	        	if (sum >= 16) {
-	        		result[index] = sum - 16;
-	            	carry = 1;
-	            	index--;
-	        	}
+			
+			if (isNegative && other.isNegative) {
+				BigHexInt copy1 = BigHexInt(digits, SIZE);
+				BigHexInt copy2 = BigHexInt(digits, SIZE);
+				BigHexInt rev_res = copy1.add(copy2);
+				BigHexInt res = BigHexInt(rev_res.digits, SIZE, true);
+				return res;
+			}
+			else if (isNegative) {
+				BigHexInt copy1 = BigHexInt(digits, SIZE);
+				BigHexInt rev_res = copy1.diff(other);
+				if (compareTo(other) > 0) {
+					BigHexInt res = BigHexInt(rev_res.digits, SIZE, true);
+					return res;
+				}
+				else if (compareTo(other) == 0) {
+					BigHexInt res = BigHexInt();
+					return res;
+				}
 				else {
-	            	carry = 0;
-	            	result[index] = sum;
-	            	index--;
-	        	}
-	        }
-	        
-	        for (int i = 0; i < SIZE; i++) {
-	        	if (result[i] >= 10) {
-	        		result[i] += 55;
+					return rev_res;
 				}
 			}
-	        
-	        if (carry == 0) {
-	            BigHexInt res = BigHexInt (result, SIZE);
-	            return res;
-	        }
+			else if (other.isNegative) {
+				BigHexInt copy2 = BigHexInt(other.digits, SIZE);
+				BigHexInt rev_res = diff(copy2);
+				if (compareTo(other) > 0) {
+					return rev_res;
+				}
+				else if (compareTo(other) == 0) {
+					BigHexInt res = BigHexInt();
+					return res;
+				}
+				else {
+					BigHexInt res = BigHexInt(rev_res.digits, SIZE, true);
+					return res;
+				}
+			}
 			else {
-	            printf("Over upperbound!");
-        	}
+				int result[SIZE] = {};
+		        int index = SIZE - 1;
+		        int carry = 0;
+		        while (index > 0) {
+		        	int sum = converted_digits[index] + other.converted_digits[index] + carry;
+		        	if (sum >= 16) {
+		        		result[index] = sum - 16;
+		            	carry = 1;
+		            	index--;
+		        	}
+					else {
+		            	carry = 0;
+		            	result[index] = sum;
+		            	index--;
+		        	}
+		        }
+		        
+		        for (int i = 0; i < SIZE; i++) {
+		        	if (result[i] >= 10) {
+		        		result[i] += 55;
+					}
+				}
+		        
+		        if (carry == 0) {
+		            BigHexInt res = BigHexInt (result, SIZE);
+		            return res;
+		        }
+				else {
+		            printf("Over upperbound!");
+	        	}
+	        }
 		}
 		
 		BigHexInt diff (BigHexInt other) {
@@ -206,9 +259,43 @@ class BigHexInt {
 				return res;
 			}
 		}
-//		
-//		// Helper method for multiplying two BigInts
-//		// Returns product of a BigHexInt with an int digit as a BigHexInt
+		
+		// Returns the subtraction of this BigHexInt to other BigHexInt
+		BigHexInt subtract (BigHexInt other) {
+			BigHexInt res;
+			if (compareTo(other) == 0) {
+				res = BigHexInt();
+			}
+			else if (isNegative && other.isNegative) {
+				if (compareTo(other) > 0) {
+					res = BigHexInt(diff(other).digits, SIZE, true);
+				}
+				else {
+					res = BigHexInt(diff(other).digits, SIZE);
+				}
+			}
+			else if ((!isNegative) && other.isNegative) {
+				BigHexInt copy2 = BigHexInt(other.digits, SIZE);	
+				res = BigHexInt(add(copy2).digits, SIZE);
+			}
+			else if (isNegative && (!other.isNegative)) {
+				BigHexInt copy1 = BigHexInt(digits, SIZE);
+				res = BigHexInt(copy1.add(other).digits, SIZE, true);
+			}
+			else {
+				if (compareTo(other) > 0) {
+					res = BigHexInt(diff(other).digits, SIZE);
+				}
+				else {
+					res = BigHexInt(diff(other).digits, SIZE, true);
+				}
+			}
+			BigHexInt resBigHexInt = BigHexInt (res.digits, SIZE, res.isNegative);
+			return resBigHexInt;
+		}
+		
+		// Helper method for multiplying two BigInts
+		// Returns product of a BigHexInt with an int digit as a BigHexInt
 		BigHexInt mul_digit(int n) {
 			int result[SIZE] = {};
 			int index = SIZE - 1;
@@ -244,15 +331,21 @@ class BigHexInt {
 		
 		// Returns product of two BigHexInts as a BigHexInt
 		BigHexInt mul(BigHexInt other) {
-			BigHexInt res = BigHexInt();
+			BigHexInt resBigHexInt = BigHexInt();
 			for (int i = SIZE - 1; i > SIZE - numSigDigits -1; i--) {
 				int mulBy = converted_digits[i];
 				BigHexInt otherProd = other.mul_digit(mulBy);
 				for (int j = i+1; j < SIZE; j++) {
 					otherProd = otherProd.mul_digit(16);
 				}
-				res = res.add(otherProd);
+				resBigHexInt = resBigHexInt.add(otherProd);
 			}
-			return res;
+			if ((isNegative && other.isNegative) || ( (!isNegative) && (!other.isNegative) ) ) {
+				return resBigHexInt;
+			}
+			else {
+				BigHexInt res = BigHexInt(resBigHexInt.digits, SIZE, true);
+				return res;
+			}
 		}
 };
